66d12e2028195270f9cdd311db07cebc27e6c53c
Skeagle137 <fuerh1379@gmail.com>
2019-11-07 12:59:10 -0500

    h

--- src/main/java/net/skeagle/smallthings/TPA.java
+++ src/main/java/net/skeagle/smallthings/TPA.java
@@ -2,28 +2,25 @@ package net.skeagle.smallthings;
 
 import co.aikar.commands.BaseCommand;
 import co.aikar.commands.annotation.*;
+import net.skeagle.smallthings.utils.TpaUtil;
+import net.skeagle.smallthings.utils.TpaUtilHere;
 import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
 
-import java.util.HashMap;
-import java.util.Map;
-import java.util.UUID;
+import java.util.*;
 
 import static net.skeagle.smallthings.STmain.say;
 
 @CommandAlias("smallthings|smallt|sthings|st")
 public class TPA extends BaseCommand {
     private STmain plugin;
-    private static Map<UUID, UUID> tpa;
-
-    public Player a;
-    public Player player;
+    private List<TpaUtil> tpaList;
+    private List<TpaUtilHere> tpaHereList;
 
     TPA(STmain st) {
         plugin = st;
-        tpa = new HashMap<>();
-        this.a = null;
-        this.player = null;
+        tpaList = new ArrayList<>();
+        tpaHereList = new ArrayList<>();
     }
 
     @Subcommand("tpa")
@@ -35,16 +32,17 @@ public class TPA extends BaseCommand {
         if (args.length < 1) {
             say(p, "&cYou must specify a player.");
         } else {
-            a = Bukkit.getPlayerExact(args[0]);
+            Player a = Bukkit.getPlayerExact(args[0]);
             if (a != null) {
-                if (p == a) {
+                if (p.getName().equalsIgnoreCase(args[0])) {
                     say(p, "&cYou cannot teleport to yourself.");
                 }
                 say(p, "&aTeleport request sent.");
                 say(a, "&a" + p.getName() + " &7is requesting to teleport to you. Do /tpaccept to accept the request or /tpdeny to deny it. This request will expire in 2 minutes.");
-                tpa.put(a.getUniqueId(), p.getUniqueId());
+                final TpaUtil tpa = new TpaUtil(p, a);
+                tpaList.add(tpa);
                 Bukkit.getScheduler().scheduleSyncDelayedTask(plugin, () ->
-                        this.killRequest(p.getUniqueId()), 120 * 20L);
+                        this.killRequest(p, a), 120 * 20L);
             } else {
                 say(p, "&cThat player is not online.");
             }
@@ -56,12 +54,17 @@ public class TPA extends BaseCommand {
     @CommandPermission("small.tpaccept")
     @Description("Accept a pending teleport request.")
     public void onTpaccept(Player p, Player a) {
-        UUID aUUID = tpa.get(a.getUniqueId());
-        if (!tpa.containsKey(p.getUniqueId())) {
+        if (!this.IsPlayerInvitedByPlayer_tphere(a, p) && !this.IsPlayerInvitedByPlayer_tpa(a, p)) {
             say(p, "&cYou do not have any pending teleport requests.");
             return;
         }
-        tpa.remove(p.getUniqueId());
+        if (this.IsPlayerInvitedByPlayer_tpa(a, p)) {
+            a.teleport(p);
+        }
+        if (this.IsPlayerInvitedByPlayer_tphere(a, p)) {
+            p.teleport(a);
+        }
+        this.DeletetpaRequest(a, p);
         say(a, "&aTeleport request accepted.");
         a.teleport(p.getLocation());
     }
@@ -70,14 +73,18 @@ public class TPA extends BaseCommand {
     @CommandAlias("tpdeny|teleportdeny")
     @CommandPermission("small.tpdeny")
     @Description("Deny a pending teleport request.")
-    public void onTpdeny(Player p) {
-        if (!tpa.containsKey(p.getName())) {
+    public void onTpdeny(Player p, Player a) {
+        if (!this.IsPlayerInvitedByPlayer_tphere(a, p) && !this.IsPlayerInvitedByPlayer_tpa(a, p)) {
             say(p, "&cYou do not have any pending teleport requests.");
             return;
         }
-        Player a = Bukkit.getPlayer(tpa.get(p.getName()));
-        tpa.remove(p.getName());
         if (a != null) {
+            if (this.IsPlayerInvitedByPlayer_tpa(a, p)) {
+                this.DeletetpaRequest(a, p);
+            }
+            if (this.IsPlayerInvitedByPlayer_tphere(a, p)) {
+                this.DeletetpahereRequest(a, p);
+            }
             say(a, "&aTeleport request rejected.");
             return;
         }
@@ -95,27 +102,57 @@ public class TPA extends BaseCommand {
         } else {
             Player a = Bukkit.getPlayerExact(args[0]);
             if (a != null) {
-                if (p.getName().equals(a.getName())) {
+                if (p.getName().equalsIgnoreCase(args[0])) {
                     say(p, "&cYou cannot teleport to yourself.");
                 }
                 say(p, "&aTeleport request sent.");
                 say(a, "&a" + p.getName() + " &7is requesting to teleport to you. Do /tpaccept to accept the request or /tpdeny to deny it. This request will expire in 2 minutes.");
-                tpa.put(a.getUniqueId(), p.getUniqueId());
+                final TpaUtilHere tpaHere = new TpaUtilHere(p, a);
+                tpaHereList.add(tpaHere);
                 Bukkit.getScheduler().scheduleSyncDelayedTask(plugin, () ->
-                        this.killRequest(p.getUniqueId()), 30 * 20L);
+                        this.killRequest(p, a), 30 * 20L);
             } else {
                 say(p, "&cThat player is not online.");
             }
         }
     }
 
-    private void killRequest(final UUID key) {
-        if (tpa.containsKey(key)) {
-            final Player p = Bukkit.getPlayer(tpa.get(key));
-            if (p != null) {
-                say(p, "&cYour teleport request timed out.");
-            }
-            tpa.remove(key);
+    private void killRequest(Player p, Player a) {
+        if (this.IsPlayerInvitedByPlayer_tphere(a, p) || this.IsPlayerInvitedByPlayer_tpa(a, p)) ;
+        if (p != null) {
+            say(p, "&cYour teleport request timed out.");
         }
+        if (this.IsPlayerInvitedByPlayer_tpa(a, p)) {
+            this.DeletetpaRequest(a, p);
+        }
+        if (this.IsPlayerInvitedByPlayer_tphere(a, p)) {
+            this.DeletetpahereRequest(a, p);
+        }
+    }
+
+    private boolean IsPlayerInvitedByPlayer_tphere(final Player p1, final Player p2) {
+        for (TpaUtilHere tpahere : tpaHereList) {
+            if (p1.getDisplayName().equals(tpahere.getSelf().getDisplayName()) && p2.getDisplayName().equals(tpahere.getTarget().getDisplayName())) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private boolean IsPlayerInvitedByPlayer_tpa(final Player p1, final Player p2) {
+        for (final TpaUtil tpa : tpaList) {
+            if (p1.getDisplayName().equals(tpa.getSelf().getDisplayName()) && p2.getDisplayName().equals(tpa.getTarget().getDisplayName())) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private void DeletetpaRequest(final Player p1, final Player p2) {
+        tpaList.removeIf(tpaData -> tpaData.getSelf().getDisplayName().equals(p1.getDisplayName()) && tpaData.getTarget().getDisplayName().equals(p2.getDisplayName()));
+    }
+
+    private void DeletetpahereRequest(final Player p1, final Player p2) {
+        tpaHereList.removeIf(tpaHereData -> tpaHereData.getSelf().getDisplayName().equals(p1.getDisplayName()) && tpaHereData.getTarget().getDisplayName().equals(p2.getDisplayName()));
     }
 }
